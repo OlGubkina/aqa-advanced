162353c2a654d535f53d0da6e2583f8290794677
Olga.Gubkina <63593441+OlGubkina@users.noreply.github.com>
2024-05-14 19:18:50 +0200

    Homework 3, lesson 4

---
+++ jsBasics/task1.js
@@ -0,0 +1,17 @@
+/*
+Завдання 1: Оголошення змінних для примітивних типів
+Створіть програму, в якій ви оголосите змінні для кожного з примітивних типів даних: рядок, число, буль, null та undefined.
+Присвойте їм значення та виведіть кожну змінну на консоль.
+ */
+
+let stringa = 'Some string';
+let number = 10;
+let isAdmin = true;
+let nullVariable = null;
+let undefinedVariable;
+
+console.log(`This is string - ${stringa}`);
+console.log(`This is number - ${number}`);
+console.log(`This is boolean - ${isAdmin}`);
+console.log(`This is null - ${nullVariable}`);
+console.log(`This is undefined - ${undefinedVariable}`);
---
+++ jsBasics/task4.js
@@ -0,0 +1,17 @@
+// 4.1: Площа кола (π * radius^2)
+let PI = Math.PI;
+let radiusOfCircle = 10.5;
+
+console.log(`Area of circle with radius = ${radiusOfCircle} => ${(PI*Math.pow(radiusOfCircle,2)).toFixed(2)}`);
+
+// 4.2 Площа прямокутника
+let length = 8.2;
+let width = 2.3;
+
+console.log(`Area of rectangle with length = ${length} and width = ${width} => ${(length * width).toFixed(2)}`);
+
+// 4.3 Об'єм циліндра (π * radius^2 * height)
+let radius = 5.2;
+let height = 7.3;
+
+console.log(`Area of cylinder with radius = ${radius} and height = ${height} => ${(PI*Math.pow(radiusOfCircle,2)*height).toFixed(2)}`);
